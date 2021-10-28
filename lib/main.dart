5a9c11f353f12de767d32cfe2559b5bf264e329d
GUKOVPC\Gukov <armor.1215@gmail.com>
2021-10-28 22:00:50 +0300

    changes in project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'circle_image.dart';
 
 void main() {
   runApp(const FlutterCard());
@@ -10,67 +10,100 @@ class FlutterCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      color: Colors.teal,
-      child: Stack(children: [
-        MainBackground(),
-        Container(
-          color: Color(0x500468b4),
-        )
-      ]),
-    );
-  }
-}
-
-class MainBackground extends StatelessWidget {
-  const MainBackground({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
     return Stack(
-      children: [
-        Image(
-          width: MediaQuery.of(context).size.width,
-          fit: BoxFit.fill,
-          image: const AssetImage('assets/images/bg_main_space.png'),
+      alignment: Alignment.center,
+      fit: StackFit.expand,
+      children: const [
+        Positioned(
+          child: Image(
+            fit: BoxFit.fill,
+            image: AssetImage('assets/images/bg_center_space.png'),
+          ),
         ),
-        Image(
-          width: MediaQuery.of(context).size.width,
-          fit: BoxFit.fill,
-          image: const AssetImage('assets/images/bg_top_wave.png'),
+        Positioned(
+          top: 0,
+          child: Image(
+            image: AssetImage('assets/images/bg_top_wave.png'),
+          ),
         ),
         Positioned(
           bottom: 0,
           child: Image(
-            width: MediaQuery.of(context).size.width,
-            fit: BoxFit.fill,
-            image: const AssetImage('assets/images/bg_bottom_wave.png'),
+            image: AssetImage('assets/images/bg_bottom_wave.png'),
           ),
         ),
-        const Positioned(
-          top: 160,
-          right: 30,
+        Positioned(
+          top: 130,
+          right: 60,
           child: Image(
             fit: BoxFit.fill,
-            image: const AssetImage('assets/images/bg_planet_saturn.png'),
+            image: AssetImage('assets/images/bg_planet_saturn.png'),
           ),
         ),
-        const Positioned(
+        Positioned(
           top: 180,
+          bottom: 180,
+          right: 5,
+          left: 5,
+          child: Image(
+            fit: BoxFit.fill,
+            image: AssetImage('assets/images/bg_space_ship.png'),
+          ),
+        ),
+        Positioned(
+          top: 40,
+          left: 70,
+          child: Image(
+            height: 70,
+            width: 70,
+            fit: BoxFit.fill,
+            image: AssetImage('assets/images/bg_darth_vader.png'),
+          ),
+        ),
+        Positioned(
+          top: 270,
+          bottom: 270,
+          right: 140,
+          left: 140,
+          child: Image(
+            height: 10,
+            width: 10,
+            fit: BoxFit.fill,
+            image: AssetImage('assets/images/bg_avatar.png'),
+          ),
+        ),
+        Positioned(
+          top: 20,
+          right: 40,
+          child: Image(
+            height: 80,
+            width: 160,
+            fit: BoxFit.fill,
+            image: AssetImage('assets/images/bg_top_text.png'),
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          right: 20,
           left: 20,
           child: Image(
-            fit: BoxFit.fill,
-            image: const AssetImage('assets/images/bg_top_logo.png'),
-          ),
+              height: 250,
+              fit: BoxFit.fill,
+              image: AssetImage('assets/images/bg_email_phone.png')),
         ),
-        const Positioned(
+        Positioned(
+          bottom: 240,
+          right: 170,
+          left: 170,
           child: Image(
-            fit: BoxFit.fill,
-            image: const AssetImage('assets/images/bg_center_logo.png'),
-          ),
-        )
+              height: 20,
+              width: 20,
+              fit: BoxFit.fill,
+              image: AssetImage('assets/images/bg_logo.png')),
+        ),
       ],
     );
   }
 }
+
+
